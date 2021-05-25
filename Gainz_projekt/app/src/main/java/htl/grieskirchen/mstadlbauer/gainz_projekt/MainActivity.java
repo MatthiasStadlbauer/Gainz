54ed124f20f3300c9070ef5ea52784a6c015076c
MatthiasStadlbauer <m.stadlbauer04@gmx.at>
2021-05-25 15:09:47 +0200

    Projekt

---
+++ Gainz_projekt/app/src/main/java/htl/grieskirchen/mstadlbauer/gainz_projekt/MainActivity.java
@@ -0,0 +1,14 @@
+package htl.grieskirchen.mstadlbauer.gainz_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
