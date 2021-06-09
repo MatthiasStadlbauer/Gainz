6aeb26b7d88c4dda4edd20a7b80671ab92953b08
Max Wintereder <Wintereder.Max@sus.htl-grieskirchen.at>
2021-06-09 16:29:28 +0200

    Challenges hinzugefügt

--- Gainz_projekt/app/src/main/java/htl/grieskirchen/mstadlbauer/gainz_projekt/ChallengeView_fragment.java
+++ Gainz_projekt/app/src/main/java/htl/grieskirchen/mstadlbauer/gainz_projekt/ChallengeView_fragment.java
@@ -3,22 +3,66 @@ package htl.grieskirchen.mstadlbauer.gainz_projekt;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 
 public class ChallengeView_fragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
 
     /**
      * Fragment für die Challenges
      */
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_challenge_view_fragment, container, false);
+
+
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager2 = view.findViewById(R.id.view_pager2);
+
+        FragmentManager fm = getFragmentManager();
+        adapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Kniebeugen"));
+        tabLayout.addTab(tabLayout.newTab().setText("Liegestütz"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sit Ups"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
         return view;
     }
 }
