3cd6766c1efd462cc6d7f29a4e059611845d7a4c
Max Wintereder <Wintereder.Max@sus.htl-grieskirchen.at>
2021-06-21 19:40:45 +0200

    Save und Load Kniebeugen und Liegestuetz

--- Gainz_projekt/app/src/main/java/htl/grieskirchen/mstadlbauer/gainz_projekt/ChallengeKniebeugen.java
+++ Gainz_projekt/app/src/main/java/htl/grieskirchen/mstadlbauer/gainz_projekt/ChallengeKniebeugen.java
@@ -1,15 +1,35 @@
 package htl.grieskirchen.mstadlbauer.gainz_projekt;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.os.Build.VERSION.SDK_INT;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,6 +51,8 @@ public class ChallengeKniebeugen extends Fragment {
 
     TextView doneKniebeugenFortgeschritten;
     EditText addKniebeugenFortgeschritten;
+
+    String fileName = "ChallengeKniebeugenFile";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -156,6 +178,105 @@ public class ChallengeKniebeugen extends Fragment {
 
         initViews(view);
 
+        if(!checkPermission()) {
+            requestPermission();
+        }
+
+        load();
+
         return view;
+    }
+
+    private boolean checkPermission() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            int result = ContextCompat.checkSelfPermission(getContext(), READ_EXTERNAL_STORAGE);
+            int result1 = ContextCompat.checkSelfPermission(getContext(), WRITE_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    private void requestPermission() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setData(Uri.parse(String.format("package:%s", new Object[]{getActivity().getApplicationContext().getPackageName()})));
+                startActivityForResult(intent, 2296);
+            } catch (Exception e) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivityForResult(intent, 2296);
+            }
+        } else {
+            //below android 11
+            ActivityCompat.requestPermissions(getActivity(), new String[]{WRITE_EXTERNAL_STORAGE}, 187);
+        }
+    }
+
+    private void save() {
+        String state = Environment.getExternalStorageState();
+        if (!state.equals(Environment.MEDIA_MOUNTED)) return;
+        File outFile = Environment.getExternalStorageDirectory();
+        String path = outFile.getAbsolutePath();
+        String fullPath = path + File.separator + fileName;
+        try {
+            PrintWriter out = new PrintWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(fullPath)));
+            out.print(doneKniebeugenAnfänger.getText().toString() + ";" + doneKniebeugenFortgeschritten.getText().toString());
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void load() {
+        String state = Environment.getExternalStorageState();
+        if (!state.equals(Environment.MEDIA_MOUNTED)) return;
+        File outFile = Environment.getExternalStorageDirectory();
+        String path = outFile.getAbsolutePath();
+        String fullPath = path + File.separator + fileName;
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fullPath)));
+
+            String line;
+            StringBuffer buffer = new StringBuffer();
+
+            while((line = in.readLine()) != null) {
+                buffer.append(line);
+            }
+
+            in.close();
+
+            String temp = buffer.toString();
+            String[] teile = temp.split(";");
+            doneKniebeugenAnfänger.setText(teile[0]);
+            doneKniebeugenFortgeschritten.setText(teile[1]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        load();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        save();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        save();
     }
 }
